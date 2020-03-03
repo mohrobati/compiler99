cd46e3091e191ee84271c715cb9eaa89feef9ab4
zosoist <moh.robati1333@gmail.com>
2020-03-03 17:56:32 +0330

    init commit

---
+++ lexer.py
@@ -0,0 +1,46 @@
+from ply import lex
+
+
+class Lexer:
+    tokens = [
+        'IF', 'WHILE', 'PRINT',
+        'LRB', 'RRB', 'LCB', 'RCB',
+        'INTEGER', 'SUM', 'SUB', 'MUL', 'DIV',
+        'LT', 'GT', 'SEMICOLON'
+    ]
+
+    # COLONS
+    t_SEMICOLON = r';'
+    # BRACKETS
+    t_LRB = r'\('
+    t_RRB = r'\)'
+    t_LCB = r'\{'
+    t_RCB = r'\}'
+    # OPERATOR
+    t_SUM = r'\+'
+    t_SUB = r'\-'
+    t_MUL = r'\*'
+    t_DIV = r'\/'
+    t_LT = r'\<'
+    t_GT = r'\>'
+    # KW
+    t_IF = r'if'
+    t_WHILE = r'while'
+    t_PRINT = r'print'
+
+    def t_INTEGER(self, t):
+        r'[-|+]?(\d+)'
+        return t
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    t_ignore = '\n \t'
+
+    def t_error(self, t):
+        raise Exception('Error at', t.value)
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        return self.lexer
---
+++ main.py
@@ -0,0 +1,10 @@
+from lexer import Lexer
+lexer = Lexer().build()
+file = open('test.txt')
+text_input = file.read()
+file.close()
+lexer.input(text_input)
+while True:
+    tok = lexer.token()
+    if not tok: break
+    print(tok)
