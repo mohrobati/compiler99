2925275b2f639b716f6dbc17cf2776de6dcc1e01
zosoist <moh.robati1333@gmail.com>
2020-07-19 14:32:08 +0430

    code generator added.

--- parser.py
+++ parser.py
@@ -1,37 +1,46 @@
 from ply import yacc
 from lexer import Lexer
+from nonTerminal import NonTerminal
+from codeGenerator import CodeGenerator
+
 
 class Parser:
 
     tokens = Lexer().tokens
 
     def __init__(self):
-        pass
+        self.tempCount = 0
+        self.codeGenerator = CodeGenerator()
 
     def p_program(self, p):
         "program : exp"
-        print('program : exp')
+        pass
 
     def p_exp_sum(self, p):
         "exp : exp SUM exp"
-        print('exp : exp SUM exp')
+        self.codeGenerator.generate_arithmetic_code(p, self.new_temp())
 
     def p_exp_sub(self, p):
         "exp : exp SUB exp"
-        print('exp : exp SUB exp')
+        self.codeGenerator.generate_arithmetic_code(p, self.new_temp())
 
     def p_exp_mul(self, p):
         "exp : exp MUL exp"
-        print('exp : exp MUL exp')
+        self.codeGenerator.generate_arithmetic_code(p, self.new_temp())
 
     def p_exp_div(self, p):
         "exp : exp DIV exp"
-        print('exp : exp DIV exp')
+        self.codeGenerator.generate_arithmetic_code(p, self.new_temp())
 
     def p_exp_integer(self, p):
         "exp : INTEGER"
-        print('exp : INTEGER', p[1])
+        p[0] = NonTerminal()
+        p[0].value = p[1]
 
+    def new_temp(self):
+        temp = "T" + str(self.tempCount)
+        self.tempCount += 1
+        return temp
 
     precedence = (
         ('left', 'SUM', 'SUB'),
